5204d5cc136a81f1684ac32469c11b3d44dc6a8d
Mike Grieve <mikegrieve15@gmail.com>
2024-07-19 22:57:03 -0700

    add note about page

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,7 +1,24 @@
 "use client";
 
 import PianoGame from "@/components/PianoGame";
+import { Button, Container, Text, Title } from "@mantine/core";
+import { useState } from "react";
 
 export default function Page() {
-  return <PianoGame devMode={true} />;
+  const [showAboutPage, setShowAboutPage] = useState(true);
+
+  if (showAboutPage) {
+    return (
+      <Container>
+        <Title>Play the Note</Title>
+        <Text>
+          Hit the correct note as it appears on screen. Try to get as many as
+          you can in 1 minute. Wrong notes subtract points.
+        </Text>
+        <Button onClick={() => setShowAboutPage(false)}>Start</Button>
+      </Container>
+    );
+  } else {
+    return <PianoGame devMode={true} />;
+  }
 }
